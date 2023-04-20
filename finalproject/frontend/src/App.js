e629b54bdbba0ab4ac00a3880cb58ae59120cbaa
Ethan <ethantam@berkeley.edu>
2023-04-19 23:59:23 -0700

    progress

--- finalproject/frontend/src/App.js
+++ finalproject/frontend/src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Routes, Link, useHistory, Navigate } from 'react-router-dom';
-<<<<<<< HEAD
 import Login from './components/login';
 import Main from './components/main';
 import Header from './components/header';
 import Class from './components/Class';
-=======
-import SignUp from './components/SignUp';
-import Main from './components/Main';
-import Header from './components/Header';
-import Login from './components/Login';
-import NewPost from './components/NewPost'
-
->>>>>>> 738aaa30f13fff4f05b5042f3bd74f5b830420a5
 
 function App() {
   return (
